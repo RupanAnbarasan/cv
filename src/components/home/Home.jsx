d9d705690f742eec0e15bcf611256181f38d7655
Rupan Anbarasan <rupananbarasan@gmail.com>
2024-01-12 12:04:44 +0530

    complete projects

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
+import './Home.css';
 import React from "react";
 import About from "../about/about";
 import Education from "../education/Education";
 import Project from "../projects/project";
 import Contact from "../contact/Mycontact";
+import Nav from "../nav/nav.jsx";
 const Home = () => {
   return (
-    <div>
+    <div >
       <div>
         <h2>Rupan anbarasan</h2>
       </div>
@@ -14,6 +16,7 @@ const Home = () => {
         <Project />
         <Education />
         <Contact />
+        <Nav/>
       </div>
     </div>
   );
--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -1,97 +1,123 @@
 import React from "react";
-
+import "./Project.css";
 const Project = () => {
   return (
     <div className="project-container">
-            <section>
-        <h2>Infozech Software Private Limited:</h2>
-        <p>
-          <strong> Technologies:</strong> Mendix, SQL Server
-        </p>
-        <p>
-          <strong>Duration:</strong> March 2023 to September 2023 - full time
-        </p>
-        <p>
-          <strong>Role:</strong> Developer{" "}
-        </p>
-        This application involved data in million, so optimization is the key
-        job here. Optimized the application for maximum speed and scalability.
-        Overview: Through the implementation of this endeavor, the company seeks
-        to efficiently manage its users, sites, and assets.
-      </section>
-      <section>
-        <h2>Compass Groups</h2>
-        <p>
-          <strong> Technologies:</strong> Mendix, SQL Server
-        </p>
-        <p>
-          <strong>Duration:</strong> March 2023 to September 2023 - full time
-        </p>
-        <p>
-          <strong>Role:</strong> Developer
-        </p>
-        This application involved data in million, so optimization is the key
-        job here. Optimized the application for maximum speed and scalability.
-        Overview: Through the implementation of this endeavor, the company seeks
-        to efficiently manage its users, sites, and assets.
-      </section>
-      <section>
-        <h2>Trane Technologies</h2>
-        <p>
-          <strong> Technologies:</strong> Mendix, SQL Server
-        </p>
-        <p>
-          <strong>Duration:</strong> March 2023 to September 2023 - full time
-        </p>
-        <p>
-          <strong>Role:</strong> Developer
-        </p>
-        This application involved data in million, so optimization is the key
-        job here. Optimized the application for maximum speed and scalability.
-        Overview: Through the implementation of this endeavor, the company seeks
-        to efficiently manage its users, sites, and assets.
-      </section>
-      <h1>Personal Projects</h1>
-      <section>
-        <h2>Library Management System</h2>
-        <p>
-          <strong>Technologies:</strong> ReactJS, CSS, NodeJS, MongoDB, REST
-          APIs
-        </p>
-        <p>
-          <strong>Overview:</strong> This Application is deal with the Books,
-          transactions, Users and Admins . The Users can check latest books
-          arrival, all books and personal transactions. The admin can issue,
-          return, add and remove book and check all transactions and specific
-          transactions. In this application the dynamic rendering, state
-          management , reusable code are implemented.
-        </p>
-      </section>
-      <section>
-        <h2>techNotes</h2>
-        Technologies: ReactJS, Redux, CSS, NodeJS, MongoDB, REST APIs
-        Description: The application's design document and contributed to
-        Front-end design and Client-side Business logic implementation.
-        Overview: This Application is deal with the notes, and Users. The
-        Customer can raise a Note. Notifications are sent out to respective
-        users for procedural actions. Further, the status of the notes where
-        monitored by the Manager
-      </section>
-      <section>
-        <h2>Quiz application</h2>
-        <p>
-          <strong>Technologies:</strong> ReactJS, Redux, CSS, NodeJS, MongoDB,
-          REST APIs
-        </p>
-        <p>
-          <strong>Description:</strong> The Quiz Application is a comprehensive
-          educational tool designed to streamline the quiz creation, management,
-          and assessment process within an academic institution. Developed using
-          a technology stack that includes React, Node.js, MongoDB, Redux, REST
-          API, and CSS, this application facilitates seamless communication
-          between administrators, teachers, and students.
-        </p>
-      </section>
+      <h1 className="Heading">Projects</h1>
+
+      <div className="projects">
+        <section className="project-section">
+          <h2>Infozech Software Pvt Ltd</h2>
+          <p className="technologies">
+          <strong>Technologies</strong> Mendix, SQL Server
+          </p>
+          <p className="duration">
+            <strong>Duration:</strong> March 2023 to September 2023 - full time
+          </p>
+          <p className="role">
+            <strong>Role:</strong> Full-stack Developer
+          </p>
+          <p className="overview">
+            <strong>Overview</strong>
+            This application involved data in million, so optimization is the
+            key job here. Optimized the application for maximum speed and
+            scalability. Overview: Through the implementation of this endeavor,
+            the company seeks to efficiently manage its users, sites, and
+            assets.
+          </p>
+        </section>
+
+        <section className="project-section">
+          <h2>Compass Groups</h2>
+          <p className="technologies">
+          <p><strong>Technologies</strong></p> Mendix, SQL Server
+          </p>
+          <p className="duration">
+            <strong>Duration:</strong> March 2023 to September 2023 - full time
+          </p>
+          <p className="role">
+            <strong>Role:</strong> Full-stack Developer
+          </p>
+          <p className="overview">
+            <strong>Overview</strong>
+            This application involved data in million, so optimization is the
+            key job here. Optimized the application for maximum speed and
+            scalability. Overview: Through the implementation of this endeavor,
+            the company seeks to efficiently manage its users, sites, and
+            assets.
+          </p>
+        </section>
+        <section className="project-section">
+          <h2>Trane Technologies</h2>
+          <p className="technologies">
+          <p><strong>Technologies</strong></p> Mendix, SQL Server
+          </p>
+          <p className="duration">
+            <strong>Duration:</strong> March 2023 to September 2023 - full time
+          </p>
+          <p className="role">
+            <strong>Role:</strong> Full-stack Developer
+          </p>
+          <p className="overview">
+            <strong>Overview</strong>
+            This application involved data in million, so optimization is the
+            key job here. Optimized the application for maximum speed and
+            scalability. Overview: Through the implementation of this endeavor,
+            the company seeks to efficiently manage its users, sites, and
+            assets.
+          </p>
+        </section>
+      </div>
+
+
+      <h1 className="Heading">Personal Projects</h1>
+      <div className="personal-projects">
+
+        <section className="project-section">
+          <h2>Library Management System</h2>
+          <p className="technologies">
+            <p><strong>Technologies</strong></p> React, CSS, node.js, MongoDB
+          </p>
+          <p className="overview">
+            <strong>Overview</strong> This Application is deal with the Books,
+            transactions, Users and Admins . The Users can check latest books
+            arrival, all books and personal transactions. The admin can issue,
+            return, add and remove book and check all transactions and specific
+            transactions. In this application the dynamic rendering, state
+            management , reusable code are implemented.
+          </p>
+        </section>
+
+        <section className="project-section">
+          <h2>Technotes</h2>
+          <p className="technologies">
+          <strong>Technologies</strong>React, Redux, CSS, node.js, MongoDB
+          </p>
+          <p className="overview">
+            <strong>Overview</strong> The application's design document and
+            contributed to Front-end design and Client-side Business logic
+            implementation. Overview: This Application is deal with the notes,
+            and Users. The Customer can raise a Note. Notifications are sent out
+            to respective users for procedural actions. Further, the status of
+            the notes where monitored by the Manager
+          </p>
+        </section>
+        <section className="project-section">
+          <h2>Quiz application</h2>
+          <p className="technologies">
+          <strong>Technologies</strong>React, Redux, CSS, node.js, MongoDB
+          </p>
+          <p className="overview">
+            <strong>Overview</strong> The Quiz Application is a comprehensive
+            educational tool designed to streamline the quiz creation,
+            management, and assessment process within an academic institution.
+            Developed using a technology stack that includes React, Node.js,
+            MongoDB, Redux, REST API, and CSS, this application facilitates
+            seamless communication between administrators, teachers, and
+            students.
+          </p>
+        </section>
+      </div>
     </div>
   );
 };
