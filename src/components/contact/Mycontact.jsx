b1d0f6794cb5d9c510413e6b9dbf4f4d85a3aec6
Rupan Anbarasan <rupananbarasan@gmail.com>
2024-01-13 19:49:52 +0530

    complete skills pendulum

--- src/components/contact/Mycontact.jsx
+++ src/components/contact/Mycontact.jsx
@@ -1,24 +1,50 @@
-import './Mycontact.css';
+import "./Mycontact.css";
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import EmailIcon from '@mui/icons-material/Email';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const Mycontact = () => {
   return (
-   
     <div className="my-contact">
-      <ul className="My-contact-ul">
-       
-        <li className="contact My-contact-l2">+91 9442955869</li>
-        <li className="contact My-contact-l3">Salem,Tamilnadu</li>
-      </ul>
-      <ul className='social-media'>
-      <li><a href='https://github.com/RupanAnbarasan' rel="noreferrer"target="_blank"><GitHubIcon className='social-icon' /></a></li>
-        <li><a href='https://www.linkedin.com/in/rupan-anbarasan-no9442955869/'rel="noreferrer"target="_blank"><LinkedInIcon className='social-icon'/></a></li>
-        <li><a href='mailto:rupananbarasan@gmail.com' rel="noreferrer" target="_blank"><EmailIcon className='social-icon'/></a></li>
-        <li><a href='https://www.google.com/maps/place/Salem,+Tamil+Nadu/@11.6537234,78.0558951,12z/data=!3m1!4b1!4m6!3m5!1s0x3babf1ccf52cba0b:0xee9989007068ca47!8m2!3d11.664325!4d78.1460142!16zL20vMDFqbmdz?entry=ttu' rel="noreferrer"target="_blank"><LocationOnIcon className='social-icon'/></a></li>
+      <ul className="social-media">
+        <li>
+          <a
+            href="https://github.com/RupanAnbarasan"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <GitHubIcon className="social-icon git" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/rupan-anbarasan-no9442955869/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <LinkedInIcon className="social-icon in" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="mailto:rupananbarasan@gmail.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <EmailIcon className="social-icon mail" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.google.com/maps/place/Salem,+Tamil+Nadu/@11.6537234,78.0558951,12z/data=!3m1!4b1!4m6!3m5!1s0x3babf1ccf52cba0b:0xee9989007068ca47!8m2!3d11.664325!4d78.1460142!16zL20vMDFqbmdz?entry=ttu"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <LocationOnIcon className="social-icon location" />
+          </a>
+        </li>
       </ul>
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,4 +1,4 @@
-import './Home.css';
+import "./Home.css";
 import React from "react";
 import About from "../MyInformation/Info";
 import Project from "../projects/project";
@@ -7,13 +7,21 @@ const Home = () => {
   return (
     <div>
       <div className="intro">
-        <h2>Hi, I'm Rupan Anbarasan</h2>
+        <h2>
+          <span>H</span>
+          <span>i</span>,
+          <span> I</span>'
+          <span>m </span> 
+          <span>R</span><span>u</span><span>p</span><span>a</span><span>n </span>
+          <span>A</span><span>n</span><span>b</span><span>a</span><span>r</span>
+          <span>a</span><span>s</span><span>a</span><span>n</span>
+        </h2>
         <h3>Full-stack web developer</h3>
       </div>
       <hr />
       <div>
         <About />
-<hr/>
+        <hr />
         <Project />
         <Contact />
       </div>
